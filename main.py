78e26463a9a50005788e46e3acc515ded397bb56
zhews <118117386+zhews@users.noreply.github.com>
2023-11-26 20:03:58 +0100

    feat: create REST API

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import uvicorn
+
+from greeter import greet
+
+app = FastAPI()
+
+
+@app.get("/", response_class=PlainTextResponse)
+def get_greeting(name: str = "") -> str:
+    output = greet(name)
+    return output 
+
+
 if __name__ == "__main__":
-    print("pygreeter")
+    uvicorn.run(app, host="0.0.0.0", port=8080)
