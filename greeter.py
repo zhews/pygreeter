e494b38fd568cde9e157ab11632fdc2bb35a8a17
zhews <118117386+zhews@users.noreply.github.com>
2023-12-14 00:51:44 +0100

    feat: add counters to track amount of greetings

--- greeter.py
+++ greeter.py
@@ -1,5 +1,17 @@
+from prometheus_client import Counter
+
+generic_counter = Counter(
+    "pygreeter_greetings_generic_total", "Greetings without a name specified."
+)
+specific_counter = Counter(
+    "pygreeter_greetings_specific_total", "Greetings with a name specified."
+)
+
+
 def greet(name: str) -> str:
     cleaned_name = name.strip()
     if len(cleaned_name) == 0:
+        generic_counter.inc()
         return "Hello World!"
+    specific_counter.inc()
     return f"Hello {name}!"
