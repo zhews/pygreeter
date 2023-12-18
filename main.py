c99ca8258d9a16b37420989b42132b790bfe0e65
zhews <118117386+zhews@users.noreply.github.com>
2023-12-18 10:42:08 +0100

    feat: add auto instrumentation with OpenTelemetry

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from prometheus_client import make_asgi_app
 import uvicorn
 
@@ -16,6 +22,15 @@ def get_greeting(name: str = "") -> str:
 
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
+
+resource = Resource(attributes={SERVICE_NAME: "pygreeter"})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="jaeger:4317", insecure=True))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+
+FastAPIInstrumentor.instrument_app(app)
 
 
 if __name__ == "__main__":
